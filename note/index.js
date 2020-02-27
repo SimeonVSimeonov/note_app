b4b97d21859d9b4f9c294d5e3b3f29472c0de63b
ssimeonoff <simeon@rubisoft.net>
2020-02-27 11:42:45 +0200

    Configure server setting

---
+++ note/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const mongoose = require('mongoose');
+
+const init = async () => {
+    // Create a server with a host and port
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    //Connect to db
+    server.app.db = mongoose.connect('mongodb+srv://simeonoff:test@cluster0-tjtwr.azure.mongodb.net/note_db?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+    //Load plugins
+    await server.register(require('./routes/note.routes'));
+
+    // Start the server
+    await server.start();
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
+
